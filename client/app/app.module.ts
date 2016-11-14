99ec916131b9c0ca9fc5560e8e4bc872070fb7a2
Chad Sheets <cjsheets@gmail.com>
2016-11-14 15:52:20 -0800

    added sidebar component framework

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent
   ],
   bootstrap: [ AppComponent ]
 })
