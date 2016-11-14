16ab4ebbb4b95bbd63616a98788728e6c5dc9b9f
Chad Sheets <cjsheets@gmail.com>
2016-11-14 15:34:02 -0800

    initial commit

---
+++ client/vendor.ts
@@ -0,0 +1,14 @@
+// Angular
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+// RxJS
+import 'rxjs';
+// Frontend Libraries
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap-material-design/css/bootstrap-material-design.min.css';
+import 'bootstrap-material-design/js/material.min.js';
