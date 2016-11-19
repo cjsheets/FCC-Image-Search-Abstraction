2fe1eed6db192043bc2ef9b36805b2ef2c6eca91
Chad Sheets <cjsheets@gmail.com>
2016-11-18 17:22:51 -0800

    add http module

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { ImageSearchComponent } from './components/image-search/image-search.component';
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
   ],
   declarations: [
     AppComponent,
