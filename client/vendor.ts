f5bb2a954d40065d701060b7101767129b7e6519
Chad Sheets <cjsheets@gmail.com>
2016-11-18 16:55:07 -0800

    setup search bar html

--- client/vendor.ts
+++ client/vendor.ts
@@ -11,6 +11,9 @@ import 'rxjs';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap-material-design/css/bootstrap-material-design.min.css';
+//import 'bootstrap-material-design/css/ripples.min.css';
 import 'bootstrap-material-design/js/material.min.js';
+// import 'bootstrap-material-design/js/ripples.min.js';
 // Custom Scripts
 import '../client/assets/js/scrollspy.js';
+import '../client/assets/js/material.js';
